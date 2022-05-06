c8774f811f9e41e6e3c7a121e1ad526c8eca8d45
rohitpatil-webonise <rohit.patil@weboniselab.com>
2022-05-06 15:05:37 +0530

    Initial Setup

---
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Rohit Patil on 06/05/22.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
